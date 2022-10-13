5d33e85486aee59b91066f82581e1eaa1b304cf3
Alexander Anseev <anseev704@gmail.com>
2022-10-12 22:26:18 -0400

    Day 2N
    - Made light and camera follow the player smoothly
    - Added score
    - Added game manager
    - Added win scenario
    - Improved movement
    
    TODO:
    - Create a simple menu
    - Make 2 polished levels
    - Polish transition between scenes

--- Trerath/Assets/Scripts/PlayerMovement.cs
+++ Trerath/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10f;
-    Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
+    public float Speed = 15;
+    private Rigidbody rb;
+
+    //[SerializeField]
+    //private float speed;
+    //public float Speed => speed;
+
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        // rb.AddForce(Physics.gravity, ForceMode.Acceleration);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        float xMove = Input.GetAxisRaw("Horizontal");
-        float zMove = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed;
+        // Move the player
+        rb.velocity =
+            transform.InverseTransformDirection(
+                new Vector3(
+                    Input.GetAxis("Horizontal") * Speed,
+                    0f,
+                    Input.GetAxis("Vertical") * Speed
+                )
+              );
     }
 }
